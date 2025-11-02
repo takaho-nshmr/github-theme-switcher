3124ce90cf482727947da3a0bfaa13c0d5ed469e
takaho.nishimura <57121259+takaho-nshmr@users.noreply.github.com>
2025-11-02 18:13:01 +0900

    Add CI workflow and migrate ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["dist/**", "scripts/**", "*.js"],
+  },
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        chrome: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+    },
+  },
+];
